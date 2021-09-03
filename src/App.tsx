4faf78489cacc6863acdc8ceeb81da4b7c10980a
Uladzislau Litvinau <uladzislau.litvinau@innowise-group.com>
2021-09-03 16:34:03 +0300

    adding linter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>New APP</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>New APP</h1>
+            <p />
+        </div>
+    );
 }
 
 export default App;
